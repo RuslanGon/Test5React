3d711da7a2c6b056efb75e1e882f3a764cda518e
RuslanGon <ruslan441006@gmail.com>
2024-05-14 16:45:24 +0300

    wer

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,11 +1,26 @@
 
-
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
 
 const NotFoundPage = () => {
+
+const [timer, setTimer] = useState(0)
+useEffect(() => {
+  const intervaId = setInterval(() => {
+    setTimer(prevTimer => prevTimer + 1)
+  },1000)
+  return () => clearInterval(intervaId)
+}, [])
+
+
+if(timer === 5) {
+  return <Navigate to='/' replace />
+}
+
   return (
     <div>
       <h2>Page you visited is not page! sorry</h2>
+      <h3>you well be redirected to home in {5-timer} seconds</h3>
       <Link to="/">Go Home</Link>
     </div>
   );
