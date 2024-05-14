7fa03edec45e5005ea8d8ccc1ea962aa6f5c7a26
RuslanGon <ruslan441006@gmail.com>
2024-05-14 15:11:18 +0300

    wer

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,16 +1,35 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { reguestProductDetailsById } from "../serveses/api"
 
 
 const ProductDetailsPage = () => {
 const {productId} = useParams
+const [productDetails, setProductDetails] = useState(null)
+
+useEffect(() => {
+async function fethProductDenails () {
+  try{
+const data = await reguestProductDetailsById(productId)
+setProductDetails(data)
+
+  }catch(error){
+    console.log(error);
+  }
+}
+fethProductDenails()
+}, [productId])
 
   return (
-    <div>
-      <img src="" alt="" />
-      <h2>Title:</h2>
-      <p>Brand:</p>
-      <p>Price:</p>
-    </div>
+    <>
+    <h1>Product list</h1>
+   {productDetails !== null &&  <div>
+      <img src={productDetails.thumbnail} alt={productDetails.title}  />
+      <h2>Title: {productDetails.title}</h2>
+      <p>Brand: {productDetails.brand}</p>
+      <p>Price: {productDetails.price}</p>
+    </div>}
+    </>
   )
 }
 
