35a87468f7c1f8477a5c7307852132bd63be50a3
RuslanGon <ruslan441006@gmail.com>
2024-05-14 15:18:06 +0300

    wer

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,36 +1,51 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useEffect, useRef, useState } from "react"
+import { Link, useLocation, useParams } from "react-router-dom"
 import { reguestProductDetailsById } from "../serveses/api"
 
 
+// import CommentPage from "./CommentPage"
+// import ReviesPage from "./ReviesPage"
+
+// const CommentPage = lazy(() => import('./CommentPage'))
+// const ReviesPage = lazy(() => import('./ReviesPage'))
+
+
 const ProductDetailsPage = () => {
-const {productId} = useParams
-const [productDetails, setProductDetails] = useState(null)
 
-useEffect(() => {
-async function fethProductDenails () {
-  try{
-const data = await reguestProductDetailsById(productId)
-setProductDetails(data)
+  const location = useLocation()
+  const backLinkRef = useRef(location.state ?? '/')
 
-  }catch(error){
-    console.log(error);
-  }
-}
-fethProductDenails()
-}, [productId])
+  const {productId} = useParams()
+
+  const [productDetails, setProductDetails] = useState(null)
+
+  useEffect(() => {
+    async function fetchProductDetails() {
+      try {
+        const data = await reguestProductDetailsById(productId);
+        setProductDetails(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchProductDetails();
+  }, [productId]);
 
   return (
-    <>
-    <h1>Product list</h1>
-   {productDetails !== null &&  <div>
-      <img src={productDetails.thumbnail} alt={productDetails.title}  />
-      <h2>Title: {productDetails.title}</h2>
-      <p>Brand: {productDetails.brand}</p>
-      <p>Price: {productDetails.price}</p>
-    </div>}
-    </>
-  )
+    <div>
+      <h1>Product details {productId}</h1>
+      <Link to={backLinkRef.current}>←Go back</Link>
+      {productDetails !== null && (
+        <div>
+          <img src={productDetails.thumbnail} alt={productDetails.title} />
+          <h3>Title: {productDetails.title} </h3>
+          <p>Brand: {productDetails.brand}</p>
+          <p>Price: {productDetails.price}</p>
+        </div>
+      )}
+    
+    </div>
+  );
 }
 
 export default ProductDetailsPage
